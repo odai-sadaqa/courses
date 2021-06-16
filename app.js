f46305f97ca77cefad7ed85d2f9b499c7bd2d57e
odaisadaqa <>
2021-06-16 12:50:59 +0000

    added new work

---
+++ app.js
@@ -0,0 +1,30 @@
+alert("You are in the right place to learning")
+
+var Choice =prompt ("Choice what do you need course?")
+
+if (Choice== "java"){
+  document.write("<p>" + "yes this is great the java language is good language"+ "</p>")
+  
+}
+else if (Choice== "python"){
+document.write("<p>" + "yes this is great the python language is good language"+ "</p>")
+  
+}
+else if (Choice== "PHP"){
+document.write("<p>" + "yes this is great the PHP language is good language"+ "</p>")
+  
+}
+else{
+alert("sorry we dont have this course now we hope to available soon")
+}
+
+var rating = prompt("you can rating our web site?")
+
+if(rating=="yes"){
+  prompt(" thank you for rating")
+}
+else{
+  alert (" dont worry we hope to rating us next time")
+}
+
+alert ("thank you to visiting our  rating website")

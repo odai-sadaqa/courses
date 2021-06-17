34ae66977aa331a068b03d1f627df3bb0f82e6c7
odaisadaqa <>
2021-06-17 12:54:39 +0000

    added new file

---
+++ app.js
@@ -0,0 +1,53 @@
+alert("You are in the right place to learning")
+
+var Choice =prompt ("Choice what do you need course?")
+
+while (Choice != "java" && Choice !="python" && Choice !="PHP"){
+  Choice = prompt ("sorry we dont have this cousre now, pleas Choice on of them java,python or PHP")
+}
+
+if (Choice== "java"){
+  var numOFpic = prompt(" how many time would like to follow this course")
+  for (var x=0; x< numOFpic; x++){
+  document.write("<div>" +
+        "<h3>" + "Java course" + "</h3>" + "<img src='https://www.valuecoders.com/blog/wp-content/uploads/2018/02/java.jpg'/>" + "</div")
+        }
+  
+}
+else if (Choice== "python"){
+  var numOFpic = prompt(" how many time would like to follow this course")
+  for (var x=0; x< numOFpic; x++){
+document.write("<div>" +
+        "<h3>" + "python course" + "</h3>" + "<img src='https://th.bing.com/th/id/R228ea53da63b645120095b6212fa19c9?rik=26RE%2fdvAf4wOTQ&pid=ImgRaw'/>" + "</div")
+      
+  }
+}
+else if (Choice== "PHP"){
+document.write("<p>" + "yes this is great the PHP language is good language"+ "</p>")
+
+var numOFpic = prompt(" how many time would like to follow this course")
+  for (var x=0; x< numOFpic; x++){
+document.write("<div>" +
+        "<h3>" + "PHP course" + "</h3>" + "<img src='https://th.bing.com/th/id/R3ab84585bdc7c8b822c98d3011fe25f1?rik=YjZ7ctxTcOC%2b1g&riu=http%3a%2f%2fsenelda.com%2fwp-content%2fuploads%2f2017%2f10%2fph.png&ehk=seFSEbwcAePmhQHELut%2fOaY0ngHJce2lJCKIDlpfVS4%3d&risl=&pid=ImgRaw'/>" + "</div")
+      
+  }
+  
+}
+else{
+alert("sorry we dont have this course now we hope to available soon")
+}
+
+
+
+
+var rating = prompt("you can rating our web site?")
+
+if(rating=="yes"){
+  prompt ("we hope to give us hight rating the rating from 1 to 5")
+  alert(" thank you for rating")
+}
+else{
+  alert (" dont worry we hope to rating us next time")
+}
+
+alert ("thank you to visiting our  rating website")

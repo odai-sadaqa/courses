604cda4e8b9950f27e99a998c08c5370361ced62
odaisadaqa <>
2021-06-20 12:28:44 +0000

    added new work

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
 alert("You are in the right place to learning")
 
-var Choice =prompt ("Choice what do you need course?")
+// var Choice =prompt ("Choice what do you need course?")
 
-<<<<<<< HEAD
+function course (){
+var Choice =prompt ("Choice what do you need course?")
 while (Choice != "java" && Choice !="python" && Choice !="PHP"){
   Choice = prompt ("sorry we dont have this cousre now, pleas Choice on of them java,python or PHP")
 }
@@ -25,49 +26,34 @@ document.write("<div>" +
 }
 else if (Choice== "PHP"){
 document.write("<p>" + "yes this is great the PHP language is good language"+ "</p>")
+}
 
-var numOFpic = prompt(" how many time would like to follow this course")
+// var numOFpic = prompt(" how many time would like to follow this course")
   for (var x=0; x< numOFpic; x++){
 document.write("<div>" +
         "<h3>" + "PHP course" + "</h3>" + "<img src='https://th.bing.com/th/id/R3ab84585bdc7c8b822c98d3011fe25f1?rik=YjZ7ctxTcOC%2b1g&riu=http%3a%2f%2fsenelda.com%2fwp-content%2fuploads%2f2017%2f10%2fph.png&ehk=seFSEbwcAePmhQHELut%2fOaY0ngHJce2lJCKIDlpfVS4%3d&risl=&pid=ImgRaw'/>" + "</div")
       
   }
-=======
-if (Choice== "java"){
-  document.write("<p>" + "yes this is great the java language is good language"+ "</p>")
-  
+
 }
-else if (Choice== "python"){
-document.write("<p>" + "yes this is great the python language is good language"+ "</p>")
-  
+course();
+
+function  ratingCourse(){
+  var num= prompt("how many star did you rate our course?")
+  for (var i = 0; i < num; i++){
+    document.write ("<img src='https://th.bing.com/th/id/OIP.HZzJ7Jwmvhuryd7yc-KXCwAAAA?pid=ImgDet&w=421&h=400&rs=1'/>")
+  }
 }
-else if (Choice== "PHP"){
-document.write("<p>" + "yes this is great the PHP language is good language"+ "</p>")
->>>>>>> f46305f97ca77cefad7ed85d2f9b499c7bd2d57e
-  
-}
-else{
-alert("sorry we dont have this course now we hope to available soon")
-}
+  ratingCourse();
+//////////klklklklkl
+// var rating = prompt("you can rating our web site?")
+// if(rating=="yes"){
+//   prompt ("we hope to give us hight rating the rating from 1 to 5")
+//   alert(" thank you for rating")
+// }
 
-<<<<<<< HEAD
-
-
-
-var rating = prompt("you can rating our web site?")
-
-if(rating=="yes"){
-  prompt ("we hope to give us hight rating the rating from 1 to 5")
-  alert(" thank you for rating")
-=======
-var rating = prompt("you can rating our web site?")
-
-if(rating=="yes"){
-  prompt(" thank you for rating")
->>>>>>> f46305f97ca77cefad7ed85d2f9b499c7bd2d57e
-}
-else{
-  alert (" dont worry we hope to rating us next time")
-}
-
+// else{
+//   alert (" dont worry we hope to rating us next time")
+// }
+// }
 alert ("thank you to visiting our  rating website")
